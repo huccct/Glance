38e568753a7313f58d9b0217e05d3401a1c14ed6
huccct <ucccth@gmail.com>
2025-03-18 21:57:27 +0000

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import type { ThreeElements } from '@react-three/fiber'
+import type * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import './App.css'
+
+function Box(props: ThreeElements['mesh']) {
+  const meshRef = useRef<THREE.Mesh>(null!)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={_event => setActive(!active)}
+      onPointerOver={_event => setHover(true)}
+      onPointerOut={_event => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
+    </mesh>
+  )
+}
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
+export default App
